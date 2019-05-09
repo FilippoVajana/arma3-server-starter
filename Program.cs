69b2532aa5a77d321e5f182607bfaea9e4331fe2
FilippoVajana <filippo.vajana@studenti.unimi.it>
2019-05-09 23:17:08 +0200

    basic server startup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace arma3_server_starter
 {
@@ -7,11 +10,38 @@ namespace arma3_server_starter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // init runner
+            var runner = new Runner();
+            runner.StartServer();
         }
     }
 
     class Runner
     {
-        
+        private string ServerFolder = @"D:\Games\Arma3\Game";
+
+        public Runner(string serverFolder=null)
+        {
+            if (serverFolder != null)
+            {
+                ServerFolder = serverFolder;
+            }
+        }
+
+        public void StartServer()
+        {
+            // find server exec
+            var exec = Directory.GetFiles(ServerFolder, @"*x64.exe", SearchOption.TopDirectoryOnly)
+                                .First();      
+
+            // start server
+            var serverProc = new Process();
+            serverProc.StartInfo.FileName = $"{exec}";
+            serverProc.StartInfo.CreateNoWindow = false;
+            serverProc.StartInfo.UseShellExecute = false;
+
+            serverProc.Start();            
+        }
     }
 }
