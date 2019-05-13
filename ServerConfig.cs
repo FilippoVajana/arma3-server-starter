086d3223a292b21a64c8316f7688cc5500b70bda
FilippoVajana <filippo.vajana@studenti.unimi.it>
2019-05-13 20:11:47 +0200

    add Newtonsoft.Json dependency

---
+++ ServerConfig.cs
@@ -0,0 +1,72 @@
+
+using System;
+
+namespace arma3_server_starter
+{
+    /// <summary>
+    /// Server specific configuration parameters.
+    /// </summary>
+    public class ServerConfig
+    {
+        public ServerConfig(string configPath=null)
+        {
+            
+        }
+
+        // parameters
+        public string ServerFolder { get; set; }
+        public string MissionsFolder { get; set; }
+        public string ModsFolder { get; set; }
+        public int HCCount { get; set; }
+        public ServerParams SParams { get; set; }
+        public HCParams HParams { get; set; }
+
+        private ServerConfig Load(string configPath)
+        {
+            throw new NotImplementedException("Config load not implemented yet.");
+        }
+
+        public void SaveConfig()
+        {
+            // build json
+            string json = JsonConvert.SerializeObject(this);
+
+            // create filename
+
+            // create file
+        }
+    }
+
+    public class ServerParams
+    {
+        public int Port { get; set; }
+        public string Config { get; set; }
+        public string Cfg { get; set; }
+        public string Profiles { get; set; }
+        public string Name { get; set; }
+        public string[] Mod { get; set; }
+        public bool FilePatching { get; set; }
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+
+    public class HCParams
+    {
+        public bool Client { get; set; }
+        public string Connect { get; set; }
+        public int Port { get; set; }
+        public string Password { get; set; }
+        public string Profiles { get; set; }
+        public string[] Mod { get; set; }
+        public bool NoSound { get; set; }
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+
+}
