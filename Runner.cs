45cf3797b04077f2b1381fc962d4d9eff8e1d8bc
FilippoVajana <filippo.vajana@studenti.unimi.it>
2019-05-25 12:39:43 +0200

    remove deprecated method

--- Runner.cs
+++ Runner.cs
@@ -65,47 +65,5 @@ namespace arma3_server_starter
                 hcP.Start();
             }
         }
-
-        /// <summary>
-        /// DEPRECATED.
-        /// Runs the server and the headless clients from hard coded parameters.
-        /// </summary>
-        public void StartServer()
-        {            
-            // mission params
-
-            // vanilla mission
-            //var serverParams = @"-port=2302 ""-config=D:\Games\Arma3\Game\TADST\KP_964_Altis\TADST_config.cfg"" ""-cfg=D:\Games\Arma3\Game\TADST\KP_964_Altis\TADST_basic.cfg"" ""-profiles=D:\Games\Arma3\Game\TADST\KP_964_Altis"" -name=KP_964_Altis -filePatching";
-            
-            // RHS mission
-            //var serverParams = @"-port=2302 ""-config=D:\Games\Arma3\Game\TADST\KP_964_RHS_Altis\TADST_config.cfg"" ""-cfg=D:\Games\Arma3\Game\TADST\KP_964_RHS_Altis\TADST_basic.cfg"" ""-profiles=D:\Games\Arma3\Game\TADST\KP_964_RHS_Altis"" -name=KP_964_RHS_Altis -filePatching ""-mod=D:\Games\Arma3\Game\mods\@RHSUSAF;D:\Games\Arma3\Game\mods\@RHSAFRE;D:\Games\Arma3\Game\mods\@Project OPFOR;D:\Games\Arma3\Game\mods\@CBA_A3""";
-
-            // Lythium RHS
-            var serverParams = @"-port=2302 ""-config=D:\Games\Arma3\Game\TADST\KP_964_RHS_Lythium\TADST_config.cfg"" ""-cfg=D:\Games\Arma3\Game\TADST\KP_964_RHS_Lythium\TADST_basic.cfg"" ""-profiles=D:\Games\Arma3\Game\TADST\KP_964_RHS_Lythium"" -name=KP_964_RHS_Lythium -filePatching ""-mod=D:\Games\Arma3\Game\mods\@Advanced AI Command;D:\Games\Arma3\Game\mods\@LYTHIUM;D:\Games\Arma3\Game\mods\@Jbad;D:\Games\Arma3\Game\mods\@RHSUSAF;D:\Games\Arma3\Game\mods\@RHSAFRE;D:\Games\Arma3\Game\mods\@Project OPFOR;D:\Games\Arma3\Game\mods\@CBA_A3""";
-
-            // start server
-            Console.WriteLine("Starting Arma3 Server");
-
-            var serverP = new Process();
-            serverP.StartInfo.FileName = $"{_serverExe}";
-            serverP.StartInfo.Arguments = $"{serverParams}";
-            serverP.Start();
-            Console.WriteLine("Server started");                       
-
-            // start headless clients            
-            int hc_num = 2;
-            var hcParams = @"-client -connect=localhost -port=2302  -nosound -password=sig4freedom -profiles=""D:\Games\Arma3\Profiles"" -mod=""D:\Games\Arma3\Game\mods\@Advanced AI Command;D:\Games\Arma3\Game\mods\@LYTHIUM;D:\Games\Arma3\Game\mods\@Jbad;D:\Games\Arma3\Game\mods\@RHSUSAF;D:\Games\Arma3\Game\mods\@RHSAFRE;D:\Games\Arma3\Game\mods\@Project OPFOR;D:\Games\Arma3\Game\mods\@CBA_A3""";
-            
-            for (int i = 0; i < hc_num; i++)
-            {                
-                Task.Delay(60000).Wait();
-                Console.WriteLine($"Starting Arma3 HC#{i}");  
-
-                var hcP = new Process();
-                hcP.StartInfo.FileName = $"{_serverExe}";
-                hcP.StartInfo.Arguments = $"{hcParams}";
-                hcP.Start();
-            }           
-        }
     }
 }
